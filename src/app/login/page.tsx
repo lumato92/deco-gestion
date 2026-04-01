f687a6acb17f26a198da2000ea1b743155486119
lumato92 <55516181+lumato92@users.noreply.github.com>
2026-04-01 12:32:57 -0300

    Fix ERROR login vercel

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,6 +4,7 @@
 
 import { useState } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
+export const dynamic = 'force-dynamic'
 
 export default function LoginPage() {
   const router = useRouter()
